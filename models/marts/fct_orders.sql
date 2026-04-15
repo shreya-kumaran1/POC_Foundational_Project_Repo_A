e2a9dab12e271eba0463d36b25f49ca56979bd86
shreya-kumaran1 <shreya.kumaran@dbtlabs.com>
2026-04-15 12:50:26 -0500

    Update fct_orders.sql

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -49,8 +49,7 @@ final as (
         order_item_summary.gross_item_sales_amount,
         order_item_summary.item_discount_amount,
         order_item_summary.item_tax_amount,
-        order_item_summary.net_item_sales_amount, 
-        'for example' as added_col
+        order_item_summary.net_item_sales_amount
     from orders
     inner join order_item_summary
         on orders.order_key = order_item_summary.order_key
